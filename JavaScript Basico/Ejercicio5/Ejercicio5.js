37061e845dd89ee516c15ca2081b294a25099fd5
Pablo Manchon <pablomanchon97@gmail.com>
2023-02-27 14:25:39 -0300

    Agrego Ejercicio 5

---
+++ JavaScript Basico/Ejercicio5/Ejercicio5.js
@@ -0,0 +1,11 @@
+let alturaCm = 172;
+
+let alturaM = 172 / 100;
+
+let pesoKg = 72.3;
+
+let metrosRed = Math.ceil(alturaM);
+
+let pesoRed = Math.floor(pesoKg);
+
+let bool = Number.MAX_VALUE + 1 == Number.MAX_VALUE;
