67a7789d6b416fdf76dddd8c7f475ff17d02663f
Pablo Manchon <pablomanchon97@gmail.com>
2023-02-27 11:38:21 -0300

    Agrego Ejercicio2

---
+++ JavaScript Basico/Ejercicio2/Ejercicio2.js
@@ -0,0 +1,11 @@
+let nombre = "Pablo";
+let edad = 25;
+let isProgramador = true;
+let fechaNacimiento = new Date("october 13 1997");
+const libro ={
+    titulo: "Así habló Zaratrusta",
+    autor: "Friedrich Nietzsche",
+    fecha: new Date("1 jan 1883"),
+    url: "https://www.worldcat.org/es/title/1004275390"
+};
+const lista = [nombre, edad, isProgramador, fechaNacimiento, libro];
