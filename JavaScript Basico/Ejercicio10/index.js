f5864184ec6a79a9bed762d8ce7c8b05d8d6dde2
Pablo Manchon <pablomanchon97@gmail.com>
2023-03-01 13:33:36 -0300

    Modifico Ejercicio 10

--- JavaScript Basico/Ejercicio10/index.js
+++ JavaScript Basico/Ejercicio10/index.js
@@ -1,8 +1,9 @@
 import * as cont from "./Modulos/Controller.js";
+import chalk from "chalk";
 
 const suma1 = cont.suma(5, 10);
 const suma2 = cont.suma(10, 10);
 
 const multiplicacion = cont.multiplica(suma1, suma2);
 
-console.log(multiplicacion);
+console.log(chalk.green(multiplicacion));
