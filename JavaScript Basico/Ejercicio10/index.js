43961c4329e5c439b4437ab4653e906fa71b67a5
Pablo Manchon <pablomanchon97@gmail.com>
2023-03-01 13:29:24 -0300

    Agrego Ejercicio 10

---
+++ JavaScript Basico/Ejercicio10/index.js
@@ -0,0 +1,8 @@
+import * as cont from "./Modulos/Controller.js";
+
+const suma1 = cont.suma(5, 10);
+const suma2 = cont.suma(10, 10);
+
+const multiplicacion = cont.multiplica(suma1, suma2);
+
+console.log(multiplicacion);
